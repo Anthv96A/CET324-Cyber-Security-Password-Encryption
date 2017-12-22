938466b1195590e2f0ccaa77edd1a657e053e675
Anthv96A <anthv73@googlemail.com>
2017-12-22 13:55:01 +0000

    ready for deployment

--- assets/app/login/login.service.ts
+++ assets/app/login/login.service.ts
@@ -16,13 +16,13 @@ export class UserService{
 
     constructor(private http: Http, private errorService: ErrorService, private successService: SuccessService){}
 
-    signup(user: User): Observable<User>{
+    signup(user: User){
         const body  = JSON.stringify(user)
         const headers = new Headers({'Content-Type':'application/json'});
         return this.http.post(this.url +'/user/', body, {headers: headers}).map(this.extractData).catch(this.catchException);
     }
 
-    signin(user: User): Observable<User>{
+    signin(user: User){
         const body  = JSON.stringify(user)
         const headers = new Headers({'Content-Type':'application/json'});
         return this.http.post(this.url +'/user/signin', body, {headers: headers}).map((data: Response) =>{ return data.json() }).catch(this.catchException);
